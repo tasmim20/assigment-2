04d7c39b69bd536e904af5dcb2fed160eeeaa1c9
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-12-08 19:24:57 +0600

    dfasd

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,36 +1,36 @@
 import { UserModel } from '../user.model'
 import { User } from './user.interface'
 
-const createUserIntoDB = async (userData: User) => {
-  // const result = await UserModel.create(user)
+// const createUserIntoDB = async (userData: User) => {
+//   // const result = await UserModel.create(user)
 
-  const user = new UserModel(userData)
-  const result = await user.save()
+//   const user = new UserModel(userData)
+//   const result = await user.save()
+//   return result
+// }
+
+// const getAllUsersFromDB = async () => {
+//   const result = await UserModel.find()
+//   return result
+// }
+// const getSingleUsersFromDB = async (id: string): Promise<User | null> => {
+//   const result = await UserModel.findOne({ userId: id })
+//   return result
+// }
+
+// const updateUser = async (id: string, userData: User): Promise<User | null> => {
+//   const result = await UserModel.findByIdAndUpdate(id, userData, {
+//     new: true,
+//     runValidators: true,
+//   })
+
   return result
 }
 
-const getAllUsersFromDB = async () => {
-  const result = await UserModel.find()
-  return result
-}
-const getSingleUsersFromDB = async (id: string): Promise<User | null> => {
-  const result = await UserModel.findOne({ userId: id })
-  return result
-}
-
-const updateUser = async (id: string, userData: User): Promise<User | null> => {
-  const result = await UserModel.findByIdAndUpdate(id, userData, {
-    new: true,
-    runValidators: true,
-  })
-
-  return result
-}
-
-const deleteUser = async (id: string) => {
-  const result = await UserModel.updateOne({ id }, { isDeleted: true })
-  return result
-}
+// const deleteUser = async (id: string) => {
+//   const result = await UserModel.updateOne({ id }, { isDeleted: true })
+//   return result
+// }
 
 export const UserServices = {
   createUserIntoDB,
